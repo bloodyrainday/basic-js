f0505640fe94e70292bc7013de1a64eec353780b
bloodyrainday <bloodyrainday@gmail.com>
2023-12-15 20:29:33 +0300

    some tasks were done

--- src/sum-digits.js
+++ src/sum-digits.js
@@ -12,9 +12,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(n) {
+  let sum = 0;
+  let sum2 = 0;
+  n.toString().split('').forEach(item => {
+    sum += +item;
+  })
+
+  if (sum >= 10) {
+    sum.toString().split('').forEach(item => {
+      sum2 += +item;
+    })
+    return sum2
+  } 
+  else {
+    return sum;
+  }
 }
 
 module.exports = {
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  let arr1 = arr;
+  if (Array.isArray(arr)) {
+    if (arr.includes('--double-next')) {
+      arr[arr.indexOf('--double-next')] = arr[arr.indexOf('--double-next') + 1];
+      return arr.sort();
+      
+    } else if (arr.includes('--discard-prev')) {
+      if (arr.indexOf('--discard-prev') == 0) {
+        arr.shift();
+        return arr;
+    } else {
+       arr.splice((arr.indexOf('--discard-prev') - 1), (arr.indexOf('--discard-prev')- 1)); 
+        return arr;
+    }
+    }
+      else if (arr.includes('--double-prev')) {
+          arr.shift();
+          return arr;
+      }
+      else if (arr.includes('undefined')) {
+          arr.splice((arr.indexOf('undefined')));
+          return arr;
+      }
+  } else if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!")
+  } 
+  return arr1;
 }
 
 module.exports = {
