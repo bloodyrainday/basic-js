5d6f1f7d2aad742ded6e0cdc6a32c26f84c772c0
Mikita Bialko <bloodyrainday@gmail.com>
2025-10-21 22:53:42 +0300

    fix: solve countCats task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require("../lib");
 
 /**
  * Given matrix where you have to find cats by ears "^^"
@@ -14,11 +14,18 @@ const { NotImplementedError } = require('../lib');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function countCats(matrix) {
+  let cats = 0;
+  matrix.forEach((item) => {
+    item.forEach((item2) => {
+      if (item2 === "^^") {
+        cats++;
+      }
+    });
+  });
+  return cats;
 }
 
 module.exports = {
-  countCats
+  countCats,
 };
